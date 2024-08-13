41789150c3e16fe665733c25b94aa946ab1a6ef6
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2024-08-12 22:18:20 -0300

    Folia First Commit

---
+++ src/test/java/br/edu/unicesumar/folia/FoliaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.folia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
