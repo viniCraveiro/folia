792b88b6bab8c96148598efe34e3a19f0f73c7b7
Luana Magnesi <luana.lr.82@gmail.com>
2024-08-20 21:59:44 -0300

    teste

--- src/main/java/br/edu/unicesumar/folia/FoliaApplication.java
+++ src/main/java/br/edu/unicesumar/folia/FoliaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FoliaApplication {
-
+//teste
 	public static void main(String[] args) {
 		SpringApplication.run(FoliaApplication.class, args);
 	}
