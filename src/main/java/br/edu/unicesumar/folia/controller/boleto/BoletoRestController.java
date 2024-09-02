8dcc8332d93faa3d9bb42d92367c33b6ff6d0a0b
GabrielLuizLupionVinhaes <gabriel.luiz.bag@gmail.com>
2024-09-02 20:13:43 -0300

    Teste unitario|Validação de CPF e CNPJ|Alteração de nomemclatura de pastas

--- src/main/java/br/edu/unicesumar/folia/controller/boleto/BoletoRestController.java
+++ src/main/java/br/edu/unicesumar/folia/controller/boleto/BoletoRestController.java
@@ -1,13 +1,52 @@
 package br.edu.unicesumar.folia.controller.boleto;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.edu.unicesumar.folia.domain.banco.Banco;
+import br.edu.unicesumar.folia.domain.banco.BancoService;
+import br.edu.unicesumar.folia.domain.usuario.Usuario;
+import br.edu.unicesumar.folia.domain.usuario.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("boleto")
 public class BoletoRestController {
 
 
+    private final BancoService bancoService;
+
+    public BoletoRestController(BancoService bancoService) {
+        this.bancoService = bancoService;
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Void> cadastrar(@RequestBody @Valid Banco banco){
+        bancoService.salvaBanco(banco);
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{uuid}")
+    public ResponseEntity<Void> deletar(@PathVariable UUID uuid){
+        bancoService.deletarBanco(uuid);
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{uuid}")
+    public ResponseEntity<Banco> atualizar(@PathVariable UUID uuid, @RequestBody Banco banco){
+        bancoService.atualizaBanco(uuid, banco);
+        return new ResponseEntity<>(banco, HttpStatus.OK);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<Banco> visualizar(@PathVariable UUID uuid){
+        Banco response = bancoService.encontrarPorIdBanco(uuid);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
 
 }
