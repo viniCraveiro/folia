67468e66373b31fbaed70b5face851dc112e7182
GabrielLuizLupionVinhaes <gabriel.luiz.bag@gmail.com>
2024-09-04 22:17:08 -0300

    Ajustes em teste unitario

--- src/test/java/br/edu/unicesumar/folia/domain/usuario/UsuarioTest.java
+++ src/test/java/br/edu/unicesumar/folia/domain/usuario/UsuarioTest.java
@@ -1,89 +1,57 @@
 package br.edu.unicesumar.folia.domain.usuario;
 
 import br.edu.unicesumar.folia.domain.endereco.Endereco;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class UsuarioTest {
 
-@WebMvcTest
-@ActiveProfiles("test")
-class UsuarioTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
+    @InjectMocks
     private UsuarioService usuarioService;
 
-    @MockBean
+    @Mock
     private UsuarioRepository usuarioRepository;
 
+    private Endereco endereco;
     private Usuario usuario;
 
     @BeforeEach
-    public void setUp() {
-        usuario = new Usuario();
-        usuario.setIdentificacao("12345678901");
-        usuario.setNome("João Silva");
-        usuario.setEmail("joao.silva@example.com");
-        Endereco endereco = new Endereco();
-        usuario.setEndereco(endereco);
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        endereco = new Endereco("Avenida Teste", "123", "Apto 101", "Rua teste", "Maringá", "01311-200", "PR");
+        usuario = new Usuario("13880494908", "Gabriel", "gabriel@example.com", endereco);
     }
 
     @Test
-    public void testCreateUsuario() throws Exception {
+    void deveSalvarUsuario() {
         when(usuarioRepository.save(any(Usuario.class))).thenReturn(usuario);
 
-        mockMvc.perform(post("/usuario")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(usuario)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.nome").value("João Silva"));
+        boolean resultado = (usuarioService.salvaUsuario(usuario) != null);
+
+        Assertions.assertTrue(resultado, "Usuário deve ser salvo!");
+        verify(usuarioRepository).save(usuario);
     }
 
     @Test
-    public void testGetUsuario() throws Exception {
-        when(usuarioRepository.findById(anyLong())).thenReturn(Optional.of(usuario));
+    void deveAtualizarNomeDoUsuario() {
+        when(usuarioRepository.findById(usuario.getId())).thenReturn(Optional.of(usuario));
+        when(usuarioRepository.save(any(Usuario.class))).thenAnswer(invocation -> invocation.getArgument(0));
 
-        mockMvc.perform(get("/usuario/1")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.nome").value("João Silva"));
+        Endereco enderecoNovo = new Endereco("Avenida Teste Atualizado", "321", "Apto 232", "Rua teste Atualizado", "Maringá", "01311-200", "PR");
+        Usuario usuarioAtualizado = new Usuario("453.263.940-90", "Luiz", "luiz@example.com", enderecoNovo);
+
+        Usuario resultado = usuarioService.atualizaUsuario(usuario.getId(), usuarioAtualizado);
+
+        Assertions.assertEquals("Luiz", resultado.getNome(), "Nome do usuário deve ser atualizado");
+        Assertions.assertEquals("Avenida Teste Atualizado", resultado.getEndereco().getLogradouro(), "Endereço do usuário deve ser atualizado");
+        verify(usuarioRepository).save(usuario);
     }
 
-    @Test
-    public void testUpdateUsuario() throws Exception {
-        usuario.setNome("João Silva Atualizado");
-        when(usuarioRepository.save(any(Usuario.class))).thenReturn(usuario);
-        when(usuarioRepository.findById(anyLong())).thenReturn(Optional.of(usuario));
-
-        mockMvc.perform(put("/usuario/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(usuario)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.nome").value("João Silva Atualizado"));
-    }
-
-    @Test
-    public void testDeleteUsuario() throws Exception {
-        when(usuarioRepository.findById(anyLong())).thenReturn(Optional.of(usuario));
-
-        mockMvc.perform(delete("/usuario/1")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent());
-    }
 }
