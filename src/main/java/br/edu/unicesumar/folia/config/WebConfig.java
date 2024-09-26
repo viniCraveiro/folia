6c532ccfec7435eed659ceeaea2975a4cabb467f
Fernando Lima <fernando.angelo39@gmail.com>
2024-09-25 21:53:52 -0300

    Cors Config

--- src/main/java/br/edu/unicesumar/folia/config/WebConfig.java
+++ src/main/java/br/edu/unicesumar/folia/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Alterar de acordo com o Fron
+                        .allowedOrigins("http://localhost:5173") // Alterar de acordo com o Front
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
