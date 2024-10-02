8c638420491b87943afbee74001da07d40483bb9
Luana Magnesi <luana.lr.82@gmail.com>
2024-10-02 13:09:52 -0300

    espero que esteja certo

---
+++ src/main/java/br/edu/unicesumar/folia/domain/contaBancaria/ContaBancaria.java
@@ -0,0 +1,50 @@
+package br.edu.unicesumar.folia.domain.contaBancaria;
+
+
+import br.edu.unicesumar.folia.domain.Entidade;
+import br.edu.unicesumar.folia.domain.banco.Banco;
+import br.edu.unicesumar.folia.domain.usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "CONTA_BANCARIA")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContaBancaria extends Entidade {
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "BANCO_FK", nullable = false)
+    private Banco banco;
+
+    @Column(nullable = false)
+    private String numeroConta;
+
+    @Column(nullable = false)
+    private String agencia;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "USUARIO_FK", nullable = false)
+    private Usuario titular;
+
+    @Column(nullable = false)
+    private String cpfCnpj;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoConta tipoConta;
+
+    @Column(nullable = false)
+    private String codigoIban;
+
+    @Column(nullable = false)
+    private String dataAbertura;
+
+    @Column
+    private String telefoneContato;
+
+    @Column
+    private String emailTitular;
+}
