8ab5fd7aef0c8b17f13ad933fb1a856a27597ed1
fernandoangelo <fernando.angelo@peon.moda>
2024-11-06 14:18:01 -0300

    fix: alterado retorno da data e uuid

--- src/main/java/br/edu/unicesumar/folia/controller/boleto/BoletoListaDTO.java
+++ src/main/java/br/edu/unicesumar/folia/controller/boleto/BoletoListaDTO.java
@@ -6,25 +6,28 @@ import br.edu.unicesumar.folia.domain.boleto.Status;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Getter
 @Setter
 public class BoletoListaDTO {
+    private String uuid;
     private Status status;
     private String nome;
     private String banco;
     private String parcela;
-    private String vencimento;
+    private LocalDate vencimento;
     private String valor;
 
     public void alimentarDados(Boleto boleto) {
+        this.uuid = boleto.getId().toString();
         if (boleto.getEmpresa() != null) {
             this.nome = boleto.getEmpresa().getNomeFantasia();
         }
         this.banco = boleto.getBanco().getNome();
         this.parcela = boleto.getTotalParcelas();
-        this.vencimento = String.valueOf(boleto.getDataVencimento());
+        this.vencimento = boleto.getDataVencimento();
         this.valor = boleto.getValor();
         this.status = boleto.getStatus();
     }
