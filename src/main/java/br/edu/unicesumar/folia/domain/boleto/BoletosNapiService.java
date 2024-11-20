35350945083b615ff552ab15e2702ffd0426a264
GabrielLuizLupionVinhaes <gabriel.luiz.bag@gmail.com>
2024-11-20 14:20:02 -0300

    Ajuste de URL

--- src/main/java/br/edu/unicesumar/folia/domain/boleto/BoletosNapiService.java
+++ src/main/java/br/edu/unicesumar/folia/domain/boleto/BoletosNapiService.java
@@ -37,7 +37,7 @@ public class BoletosNapiService {
     }
 
     public List<BoletoNapiDTO> consultarNapiEProcessarDados() {
-        String url = "https://napi.service.dev.peon.tec.br/folia/v0/contareceber/?token=e66d98a2-28dd-4f88-b90c-916d3f4899ba";
+        String url = "";
         RestTemplate restTemplate = new RestTemplate();
 
         try {
