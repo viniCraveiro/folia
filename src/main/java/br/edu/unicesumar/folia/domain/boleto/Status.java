a36ecc11ed7076f55c7687cce89cb8fb9bb9c645
GabrielLuizLupionVinhaes <gabriel.luiz.bag@gmail.com>
2024-11-19 22:04:20 -0300

    Salvando API Json

--- src/main/java/br/edu/unicesumar/folia/domain/boleto/Status.java
+++ src/main/java/br/edu/unicesumar/folia/domain/boleto/Status.java
@@ -1,8 +1,27 @@
 package br.edu.unicesumar.folia.domain.boleto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Status {
     ABERTO,
+    ABERTA,
     PAGO,
     VENCIDO;
 
+    @JsonCreator
+    public static Status fromString(String value) {
+        if (value != null) {
+            switch (value.toUpperCase()) {
+                case "ABERTA":
+                    return ABERTO;
+                case "PAGO":
+                    return PAGO;
+                case "VENCIDO":
+                    return VENCIDO;
+                default:
+                    throw new IllegalArgumentException("Status desconhecido: " + value);
+            }
+        }
+        return null; // ou ABERTO, caso queira retornar um valor padrão
+    }
 }
